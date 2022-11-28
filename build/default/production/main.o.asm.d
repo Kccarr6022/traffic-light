7130fcea169bf23ccbff0462d82ed57f1c1e549f
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-11-28 17:19:50 -0500

    Added lights in data direction register for EAST/WEST lights

--- build/default/production/main.o.asm.d
+++ build/default/production/main.o.asm.d
@@ -1,2 +1,2 @@
 build/default/production/main.o: \
- C:\Users\Kaden\AppData\Local\Temp\ccyrPgzh.s
+ C:\Users\Kaden\AppData\Local\Temp\ccdpYTj7.s
--- main.s
+++ main.s
@@ -1,16 +1,41 @@
 #define __SFR_OFFSET 0
 #include <avr/io.h>
+	
+
+setup:				; Set PB2 as OUTPUT
+	
+	
+	;EAST/WEST
+	
 	; Data Direction Register Before
 	; PB7	PB6	PB5	PB4	PB3	PB2	PB1	PB0
 	;x	x	x	x	x	x	x	x
-
-setup:				; Set PB2 as OUTPUT
+	
+	;Green light
+	sbi	DDRB, 1
+	cbi	PORTB, 1
+	
+	;Yellow light
           sbi       DDRB, 2          ; Command: (sbi = Set bit in I/O register) Destination: (DDRB = A I/O register) Source: (bit_num = a number to make DDRB an output pin)
           cbi       PORTB,2        ; turns LED off (cbi = clear bit in I/O register)
           
+	; Red light
+	sbi	DDRB, 3
+	cbi	PORTB, 3
+	
+	
+	
 	; Data Direction Register After
 	; PB7	PB6	PB5	PB4	PB3	PB2	PB1	PB0
-	;x	x	x	x	x	1	x	x
+	;x	x	x	x	1	1	1	x
+	
+	;NORTH/SOUTH
+	
+
+
+
+
+
 loop:
 turn_led_on:
           sbi       PORTB,2        ; turn led on
@@ -22,6 +47,18 @@ turn_led_off:
           
           rjmp loop
 
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
 wait_25:
           ldi       r18,16
 wait_25_1:
